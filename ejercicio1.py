4342d06ea9397183db50865ed5cb11d5361fd31f
Franlop24 <franlopbri@gmail.com>
2024-10-10 09:55:02 -0600

    01 Primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+'''
+Ejercicio 1:
+    Escribir un programa que pregunte al usuario su nombre
+    e imprima un saludo
+Entradas:
+    nombre: string
+Salida:
+    saludo: string
+'''
+nombre = input('Escribe tu nombre: ')
+edad = input('Escribe tu edad: \n')
+saludo = "Holaa " + nombre + " de " + edad + " años!"
+print(saludo)
